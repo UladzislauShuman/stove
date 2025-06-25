4734815af07d20dfee7914299766676db400783b
UladzislauLinux <fpm.shumanVO@bsu.by>
2025-06-25 15:26:55 +0300

    Set up the environments

---
+++ backend/stove-app-parent/src/main/java/by/shumpanov/stove/stove_app_parent/StoveAppApplication.java
@@ -0,0 +1,13 @@
+package by.shumpanov.stove.stove_app_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoveAppApplication.class, args);
+	}
+
+}
---
+++ backend/stove-app-parent/src/test/java/by/shumpanov/stove/stove_app_parent/StoveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shumpanov.stove.stove_app_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
